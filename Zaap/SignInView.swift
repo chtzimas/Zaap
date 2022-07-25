6fa655a1e9ae2f314b7fe1db092a19433ceacfdb
Christos Tzimas <tzimas13@hotmail.com>
2022-07-25 17:06:24 +0300

    add sign in to sign up navigation

--- Zaap/SignInView.swift
+++ Zaap/SignInView.swift
@@ -11,26 +11,31 @@ struct SignInView: View {
     @StateObject var viewModel = DependencyInjector.shared.resolve(type: SignInViewModel.self)!
     
     var body: some View {
-        VStack {
-            welcomeView
-            UserDetailView(userDetailText: $viewModel.email, placeholder: L10n.emailPlaceholder)
-            UserDetailView(userDetailText: $viewModel.password, placeholder: L10n.passwordPlaceholder)
-            signInButtonView
-                .padding(.vertical, 20)
-            forgotPasswordButtonView
-            LabelledDividerView(label: L10n.or)
-                .padding(.top, 30)
-            HStack {
-                oathSignInButtonView(with: .google)
-                    .padding(.trailing, 20)
-                oathSignInButtonView(with: .facebook)
-                    .padding(.leading, 20)
-            }
-            .padding(.top, 20)
-            signUpButtonView
+        NavigationView {
+            VStack {
+                welcomeView
+                UserDetailView(userDetailText: $viewModel.email, placeholder: L10n.emailPlaceholder)
+                UserDetailView(userDetailText: $viewModel.password, placeholder: L10n.passwordPlaceholder)
+                signInButtonView
+                    .padding(.vertical, 20)
+                forgotPasswordButtonView
+                LabelledDividerView(label: L10n.or)
+                    .padding(.top, 30)
+                HStack {
+                    oathSignInButtonView(with: .google)
+                        .padding(.trailing, 20)
+                    oathSignInButtonView(with: .facebook)
+                        .padding(.leading, 20)
+                }
+                .padding(.top, 20)
+                HStack {
+                    dontHaveAnAccountView
+                    signUpButtonView
+                }
                 .padding(.top, 40)
+            }
+            .padding(.horizontal, 30)
         }
-        .padding(.horizontal, 30)
     }
     
     private var welcomeView: some View {
@@ -57,11 +62,14 @@ struct SignInView: View {
         }
     }
     
+    private var dontHaveAnAccountView: some View {
+        Text(L10n.donTHaveAnAccount)
+            .foregroundColor(.gray)
+    }
+    
     private var signUpButtonView: some View {
-        HStack {
-            Text(L10n.donTHaveAnAccount)
-                .foregroundColor(.gray)
-            Button(action: viewModel.signUp) {
+        Button(action: viewModel.signUp) {
+            NavigationLink(destination: SignUpView()) {
                 Text(L10n.signUp)
                     .fontWeight(.bold)
                     .tint(.orange)
